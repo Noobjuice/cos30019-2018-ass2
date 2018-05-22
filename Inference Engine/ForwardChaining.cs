259795ae93a005cf2abf46253172f41e884ab8f4
Manan Rajpal <100864824@student.swin.edu.au>
2018-05-22 21:17:54 +1000

    Completed Forward Chaining

--- Inference Engine/ForwardChaining.cs
+++ Inference Engine/ForwardChaining.cs
@@ -27,7 +27,7 @@ namespace InferenceEngine
 
 		}
 
-        public void initializeValues()
+        protected void initializeValues()
         {
             for (int i = 0; i < ask.Length; i++)
             {
@@ -47,11 +47,10 @@ namespace InferenceEngine
             }
         }
 
-        public bool containsSymbolInPremise(String clause, String symbol)
+        protected bool containsSymbolInPremise(String clause, String symbol)
         {
             //Separate the implication into premise and head
-            String[] separator = { "=>" };
-            String[] components = clause.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            String[] components = DifferentiateComponents(clause);
             String premise = components[0];
             String head = components[1];
             if (premise.Contains("&"))
@@ -68,9 +67,55 @@ namespace InferenceEngine
             }
         }
 
+        protected string[] DifferentiateComponents(String clause)
+        {
+            String[] separator = { "=>" };
+            string[] components = clause.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            return components;
+        }
+
         public override bool Infer()
         {
             //TODO: Finish This
+            while (agenda.Count != 0)
+            {
+                //get the first fact to process through the clauses
+                String symbol = agenda[0];
+                Console.WriteLine(symbol);
+                //pop the item of the list
+                agenda.RemoveAt(0);
+                //Add this to inferred list as this symbol is being processed
+                inferred.Add(symbol);
+                //check if the fact matches the query
+                if (symbol == tell)
+                {
+                    return true;
+                }
+                foreach (String clause in clauses)
+                {
+                    Console.WriteLine(clause);
+                    int i = 0;
+                    //check if the symbol exist within any premise among all clauses in knowledge base
+                    if (containsSymbolInPremise(clause, symbol))
+                    {
+                        //if yes then deduct the count by 1 and if the count is equal to 0 then add the head of the implication to the agenda
+                        count[i] = count[i] - 1;
+                        if (count[i] == 0)
+                        {
+                            Console.WriteLine("One premise completed");
+                            String[] components = DifferentiateComponents(clause);
+                            String head = components[1];
+                            //check again if the head matches the query
+                            if (head.Equals(tell))
+                            {
+                                return true;
+                            }
+                            agenda.Add(head);
+                        }
+                    }
+                    i++;
+                }
+            }
             return false;
         }
     }
